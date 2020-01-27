52a8d8f8f3ded913cbea19c9f5543a1252471f9f
Thiago Araujo <worf.mogh@gmail.com>
2020-01-26 16:32:20 -0800

    complete graph with missing edges (ruby)

---
+++ algorithms/graphs/complete_graph_missing_edges.rb
@@ -0,0 +1,47 @@
+require "test/unit/assertions"
+include Test::Unit::Assertions
+
+# given `n` as the number of nodes in a network
+# and `edges` as the list of edges connecting two nodes
+# find the list of missing edges to be added to the graph
+# to make it a complete undirected graph
+# where every node is connected to all the others
+
+def missing_edges(n, edges)
+  all_edges = (0...n).to_a.combination(2).to_a
+  all_edges - edges.map(&:sort)
+end
+
+# trivial graph
+def trivial_graph_test
+    n, edges = 1, []
+    actual = missing_edges(n, edges)
+    expected = []
+    assert_equal(actual, [])
+end
+
+# no missing edges
+def no_missing_edges_test
+    n = 3
+    edges = [[0,1], [1,2], [0,2]]
+    actual = missing_edges(n, edges)
+    expected = []
+
+    assert_equal actual, expected
+end
+
+# some missing edges
+def some_missing_edges_test
+    n = 4
+    edges = [[0,1], [1,2], [2,0]]
+    actual = missing_edges(n, edges)
+    expected = [[0,3], [1,3], [2,3]]
+
+    assert_equal(actual, expected)
+end
+
+# run test suite
+
+trivial_graph_test
+no_missing_edges_test
+some_missing_edges_test
